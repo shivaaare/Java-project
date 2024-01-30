1ff167f0915a5e590936215627a486883d6e0c68
shiva aare <shivaaare@shivas-Air.lan>
2024-01-29 20:27:34 -0600

    new

---
+++ src/oops/Student.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class Student {
+    String name="Shiva";
+    public String getName() {
+
+        return name;
+    }
+    public static void main(String[] args)
+    {
+        Student s=new Student();
+        System.out.println(" "+s.getName());
+    }
+}
+
+
